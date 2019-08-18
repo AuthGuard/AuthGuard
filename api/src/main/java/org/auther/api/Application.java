cab003a37f7636c0b852b512f4aafcc8682bf75c
KLD <kld_at7@hotmail.com>
2019-08-18 17:17:05 +0100

    Logback support

--- api/src/main/java/org/auther/api/Application.java
+++ api/src/main/java/org/auther/api/Application.java
@@ -6,13 +6,31 @@ import com.google.inject.Injector;
 import io.javalin.Javalin;
 import org.auther.api.routes.PermissionsRoute;
 import org.auther.api.routes.UsersRoute;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static io.javalin.apibuilder.ApiBuilder.path;
 
 public class Application {
+    private final static Logger log = LoggerFactory.getLogger(Application.class.getSimpleName());
+
     public static void main(final String[] args) {
-        final Javalin app = Javalin.create().start(3000);
+        final Javalin app = Javalin.create()
+                .start(3000);
         final Injector injector = Guice.createInjector(new InjectorModule());
+
+        app.before(context -> context.attribute("time", System.currentTimeMillis()));
+
+        app.after(context -> {
+            final Long now = System.currentTimeMillis();
+            final Long start = context.attribute("time");
+
+            if (start == null) {
+                log.info("{} {} - {}", context.method(), context.path(), context.status());
+            } else {
+                log.info("{} {} - {} {} ms", context.method(), context.path(), context.status(), now - start);
+            }
+        });
 
         app.routes(() -> {
             path("/users", injector.getInstance(UsersRoute.class));
