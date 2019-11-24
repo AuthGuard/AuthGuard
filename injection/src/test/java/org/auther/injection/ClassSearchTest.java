e7f21f09a1c6ecef3ec09c22477a050092657ac2
KLD <kld_at7@hotmail.com>
2019-11-24 13:00:32 +0000

    Fixed ClassSearchTest bug
    There were multiple implementations of the same interfaces which led
    to having one of them get picked up randomly, making the tests pass
    or fail unexpectedly.

--- injection/src/test/java/org/auther/injection/ClassSearchTest.java
+++ injection/src/test/java/org/auther/injection/ClassSearchTest.java
@@ -3,8 +3,11 @@ package org.auther.injection;
 import com.google.inject.AbstractModule;
 import org.auther.dal.AccountsRepository;
 import org.auther.dal.PermissionsRepository;
+import org.auther.dal.mock.MockAccountsRepository;
+import org.auther.dal.mock.MockPermissionsRepository;
 import org.auther.dal.model.AccountDO;
 import org.auther.dal.model.PermissionDO;
+import org.auther.emb.MessagePublisher;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.reflections.util.ClasspathHelper;
@@ -25,7 +28,7 @@ class ClassSearchTest {
         final Implementaion<AccountsRepository> implementation = classSearch.findAccountsRepositoryImplementation();
 
         assertThat(implementation).isNotNull();
-        assertThat(implementation.getImplementationClass()).isEqualTo(AccountsRepositoryImpl.class);
+        assertThat(implementation.getImplementationClass()).isEqualTo(MockAccountsRepository.class);
         assertThat(implementation.getInjectorModule()).isEqualTo(AccountsRepositoryInjector.class);
     }
 
@@ -33,7 +36,7 @@ class ClassSearchTest {
     void findAccountsRepositoryNoImplementation() {
         final Reflections reflections = new Reflections(new ConfigurationBuilder()
                 .setUrls(ClasspathHelper.forClass(this.getClass()))
-                .filterInputsBy(new FilterBuilder().exclude(".*" + AccountsRepositoryImpl.class.getSimpleName() + ".*"))
+                .filterInputsBy(new FilterBuilder().exclude(".*" + MockAccountsRepository.class.getSimpleName() + ".*"))
         );
 
         final ClassSearch classSearch = new ClassSearch(reflections);
@@ -47,7 +50,7 @@ class ClassSearchTest {
         final Implementaion<PermissionsRepository> implementation = classSearch.findPermissionsRepositoryImplementation();
 
         assertThat(implementation).isNotNull();
-        assertThat(implementation.getImplementationClass()).isEqualTo(PermissionsRepositoryImpl.class);
+        assertThat(implementation.getImplementationClass()).isEqualTo(MockPermissionsRepository.class);
         assertThat(implementation.getInjectorModule()).isEqualTo(PermissionsRepositoryInjector.class);
     }
 
@@ -55,7 +58,7 @@ class ClassSearchTest {
     void findPermissionsRepositoryNoImplementation() {
         final Reflections reflections = new Reflections(new ConfigurationBuilder()
                 .setUrls(ClasspathHelper.forClass(this.getClass()))
-                .filterInputsBy(new FilterBuilder().exclude(".*" + PermissionsRepositoryImpl.class.getSimpleName() + ".*"))
+                .filterInputsBy(new FilterBuilder().exclude(".*" + MockPermissionsRepository.class.getSimpleName() + ".*"))
         );
 
         final ClassSearch classSearch = new ClassSearch(reflections);
@@ -66,63 +69,20 @@ class ClassSearchTest {
     // Mock classes for tests
 
     // --- AccountsRepository
-    private static class AccountsRepositoryImpl implements AccountsRepository {
-        @Override
-        public AccountDO save(final AccountDO account) {
-            return null;
-        }
-
-        @Override
-        public Optional<AccountDO> getById(final String accountId) {
-            return Optional.empty();
-        }
-
-        @Override
-        public Optional<AccountDO> update(final AccountDO account) {
-            return Optional.empty();
-        }
-    }
-
     interface AccountsRepositoryNotImpl extends AccountsRepository {}
 
     @InjectorModule(target = AccountsRepository.class)
     private static class AccountsRepositoryInjector extends AbstractModule {}
 
     // --- PermissionsRepository
-    private static class PermissionsRepositoryImpl implements PermissionsRepository {
-        @Override
-        public PermissionDO save(final PermissionDO permission) {
-            return null;
-        }
-
-        @Override
-        public Optional<PermissionDO> getById(final String id) {
-            return Optional.empty();
-        }
-
-        @Override
-        public Optional<PermissionDO> search(final String group, final String name) {
-            return Optional.empty();
-        }
-
-        @Override
-        public Optional<PermissionDO> delete(final String id) {
-            return Optional.empty();
-        }
-
-        @Override
-        public Collection<PermissionDO> getAll() {
-            return null;
-        }
-
-        @Override
-        public Collection<PermissionDO> getAllForGroup(final String group) {
-            return null;
-        }
-    }
 
     interface PermissionsRepositoryNotImpl extends PermissionsRepository {}
 
     @InjectorModule(target = PermissionsRepository.class)
     private static class PermissionsRepositoryInjector extends AbstractModule {}
+
+    // --- EMB
+    private static class MessagePublisherImpl implements MessagePublisher {}
+
+    interface MessagePublisherNotImpl extends MessagePublisher {}
 }
