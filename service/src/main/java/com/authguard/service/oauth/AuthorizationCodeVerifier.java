1a006857d44d40da4f862f865dc7784956992714
Khaled Y.M <kld_at7@hotmail.com>
2020-04-19 16:24:20 +0100

    OAuth 2.0 authorization code
    Added support for authorization to support the authorization code
    flow of OAuth 2.0.

---
+++ service/src/main/java/com/authguard/service/oauth/AuthorizationCodeVerifier.java
@@ -0,0 +1,32 @@
+package com.authguard.service.oauth;
+
+import com.authguard.dal.AccountTokensRepository;
+import com.authguard.dal.model.AccountTokenDO;
+import com.authguard.service.AuthTokenVerfier;
+import com.authguard.service.exceptions.ServiceAuthorizationException;
+import com.google.inject.Inject;
+
+import java.time.ZonedDateTime;
+import java.util.Optional;
+
+public class AuthorizationCodeVerifier implements AuthTokenVerfier {
+    private final AccountTokensRepository accountTokensRepository;
+
+    @Inject
+    public AuthorizationCodeVerifier(final AccountTokensRepository accountTokensRepository) {
+        this.accountTokensRepository = accountTokensRepository;
+    }
+
+    @Override
+    public Optional<String> verifyAccountToken(final String token) {
+        final AccountTokenDO accountToken = accountTokensRepository.getByToken(token)
+                .join()
+                .orElseThrow(() -> new ServiceAuthorizationException("Invalid authorization code " + token));
+
+        if (accountToken.getExpiresAt().isBefore(ZonedDateTime.now())) {
+            throw new ServiceAuthorizationException("The authorization code has expired");
+        }
+
+        return Optional.of(accountToken.getAssociatedAccountId());
+    }
+}
