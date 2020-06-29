a3c785edcde7282c89d306784988451f6063dedc
Khaled Y.M <kld_at7@hotmail.com>
2020-06-29 19:25:50 +0100

    Change JTI provider to use AccountTokensRepository
    Seriously how was it still using a set?

--- service/src/test/java/com/authguard/service/jwt/BasicJtiProviderTest.java
+++ service/src/test/java/com/authguard/service/jwt/BasicJtiProviderTest.java
@@ -1,19 +1,40 @@
 package com.authguard.service.jwt;
 
+import com.authguard.dal.AccountTokensRepository;
+import com.authguard.dal.model.AccountTokenDO;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 
 class BasicJtiProviderTest {
-    private final BasicJtiProvider provider = new BasicJtiProvider();
+    private AccountTokensRepository repository;
+    private BasicJtiProvider provider;
+
+    @BeforeEach
+    void setup() {
+        repository = Mockito.mock(AccountTokensRepository.class);
+        provider = new BasicJtiProvider(repository);
+    }
 
     @Test
     void generate() {
+        Mockito.when(repository.save(any()))
+                .thenReturn(CompletableFuture.completedFuture(AccountTokenDO.builder().build()));
+
         assertThat(provider.next()).isNotNull();
     }
 
     @Test
     void notGenerated() {
+        Mockito.when(repository.getByToken(any()))
+                .thenReturn(CompletableFuture.completedFuture(Optional.empty()));
+
         assertThat(provider.validate("malicious")).isFalse();
     }
 }
