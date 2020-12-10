79cb3da4a173630c0e665339854e46fcdd55bc03
Khaled Y.M <kld_at7@hotmail.com>
2020-12-10 09:20:03 +0300

    Fix RxJava subscription termination
    RxJava treats exceptions thrown by a subscriber as termination
    events, causing the stream to no longer be active. This commit
    fixes this issue by wrapping subscribers around a safe try-catch
    block.

--- emb/src/main/java/com/authguard/emb/rxjava/RxPublisher.java
+++ emb/src/main/java/com/authguard/emb/rxjava/RxPublisher.java
@@ -3,9 +3,14 @@ package com.authguard.emb.rxjava;
 import com.authguard.emb.MessagePublisher;
 import com.authguard.emb.MessageSubscriber;
 import com.authguard.emb.model.Message;
+import io.reactivex.rxjava3.functions.Consumer;
 import io.reactivex.rxjava3.subjects.PublishSubject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class RxPublisher implements MessagePublisher {
+    private static final Logger LOG = LoggerFactory.getLogger(RxPublisher.class);
+
     private PublishSubject<Message> subject = PublishSubject.create();
 
     @Override
@@ -15,6 +20,17 @@ public class RxPublisher implements MessagePublisher {
 
     @Override
     public void acceptSubscriber(final MessageSubscriber subscriber) {
-        subject.subscribe(subscriber::onMessage);
+        subject.subscribe(safeConsumer(subscriber));
+    }
+
+    private Consumer<Message> safeConsumer(MessageSubscriber subscriber) {
+        return message -> {
+            try {
+                subscriber.onMessage(message);
+            } catch (Throwable e) {
+                LOG.warn("Subscriber {} threw an exception. This violates the message subscriber specifications and " +
+                        "needs to be fixed", subscriber.getClass(), e);
+            }
+        };
     }
 }
